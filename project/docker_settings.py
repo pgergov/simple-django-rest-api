3a8ccb9170ebef995a8dc3ef263dfcddb0478f5b
Pavlin Gergov <gergov.pavlin@gmail.com>
2017-09-03 15:03:35 +0300

    Add docker settings

---
+++ project/docker_settings.py
@@ -0,0 +1,12 @@
+from .settings import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
